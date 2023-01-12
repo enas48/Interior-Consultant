de3b8c7835b2d87ab867a244fee54d36fe7ca6e2
enas48 <enasm4829@gmail.com>
2023-01-12 15:39:02 +0200

    add

--- index.js
+++
@@ -1,9 +0,0 @@
-const express=require('express');
-const app=express();
-const port=process.env.PORT || 3000;
-
-app.use(express.static(__dirname + '/src'));
-
-const server=app.listen(port,()=>{
-    console.log('server is running');
-})
